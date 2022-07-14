6faa1ed726029834d6e8a5fb77b16d67074375df
Juan Martin Gimenez <j.martin.utn@gmail.com>
2022-07-14 16:03:20 -0300

    Widget Dolar Binance p2p

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # widget-dolar-binance-p2p
+
+Este es un widget de Scriptable que muestra la cotizacion del USDT de Binance P2P.
+
+ <p>
+   <a href="#setup">Instalación</a> •
+   <a href="#links">Links</a> •
+   <a href="#contribution">Contribution</a> •
+ </p>
+
+## Setup
+
+1.  (Si aun no lo tienes) Descarla la App de scriptable desde [AppStore](https://apps.apple.com/us/app/scriptable/id1405459188)
+2.  En la App Scriptable crea un nuevo script
+3.  Copia el codigo del archivo script.js [script.js]()
+4.  Crea un widget en la pantalla de inicio. Dejo un ejemplo en el siguiente video
+
+## Links
+
+`(Dólar USDT en Binance. Promedio de ordenes abiertas hasta llegar a 2000 USD)`
+
+-   API Price Data: https://dolar-api.gabrielzim.com/rates/single/usdt/ars
+
+## Contribution
+
+If you have any ideas or changes just let me know.
+
+## License
+
+MIT © [ann0nip](https://github.com/ann0nip)
---
+++ script.js
@@ -0,0 +1,94 @@
+// Dolar Binance p2p Widget v1.0.0
+// Author: @ann0nip
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+const params = args.widgetParameter ? args.widgetParameter.split(',') : [];
+const isDarkTheme = params?.[0] === 'dark';
+const padding = 3;
+
+const widget = new ListWidget();
+widget.url = 'http://p2p.binance.com/en/trade/sell/USDT?fiat=ARS&payment=ALL';
+widget.backgroundColor = Color.darkGray();
+widget.setPadding(padding, padding, padding, padding);
+
+const headerStack = widget.addStack();
+
+addCenteredDataView(headerStack, 'Dólar Binance P2P', 16, Color.orange());
+
+async function buildWidget() {
+    const tetherPriceInfo = await getTokenPriceInfo();
+
+    const roundedTetherBidPrice = Math.round(tetherPriceInfo.compra);
+    const roundedTetherAskPrice = Math.round(tetherPriceInfo.venta);
+
+    addCrypto('Compra', roundedTetherBidPrice, tetherPriceInfo.yesterdayBid);
+    addCrypto('Venta', roundedTetherAskPrice, tetherPriceInfo.yesterdayAsk);
+
+    const footerStack = widget.addStack();
+
+    const event = new Date(tetherPriceInfo.time);
+    const options = { timeStyle: 'medium', dateStyle: 'short' };
+    const date = event.toLocaleString('es-AR', options);
+
+    addCenteredDataView(footerStack, date, 12, Color.lightGray());
+}
+
+function addCenteredDataView(widget, data, textSize, textColor) {
+    const viewStack = widget.addStack();
+    viewStack.layoutHorizontally();
+
+    viewStack.addSpacer();
+
+    const label = viewStack.addText(data);
+    label.font = Font.regularMonospacedSystemFont(textSize);
+    label.textColor = textColor || Color.white();
+
+    viewStack.addSpacer();
+}
+
+function addCrypto(spreadText, price, grow) {
+    const rowStack = widget.addStack();
+
+    rowStack.setPadding(10, 7, 10, 7);
+    rowStack.layoutHorizontally();
+
+    let spreadLabel = rowStack.addText(spreadText);
+    spreadLabel.font = Font.boldSystemFont(15);
+    spreadLabel.textColor = Color.white();
+
+    rowStack.addSpacer();
+    let priceLabel = rowStack.addText(`$${price}`);
+    priceLabel.font = Font.boldSystemFont(15);
+
+    if (grow < price) {
+        priceLabel.textColor = Color.green();
+    } else {
+        priceLabel.textColor = Color.red();
+    }
+}
+
+async function getTokenPriceInfo(tokenId) {
+    const url = `https://dolar-api.gabrielzim.com/rates/single/usdt/ars`;
+    const req = new Request(url);
+    const apiResult = await req.loadJSON();
+
+    return {
+        compra: apiResult.bid,
+        yesterdayBid: apiResult.yesterdayBid,
+        venta: apiResult.ask,
+        yesterdayAsk: apiResult.yesterdayAsk,
+        info: apiResult.description,
+        time: apiResult.time,
+    };
+}
+
+async function loadImage(imgUrl) {
+    const req = new Request(imgUrl);
+    return await req.loadImage();
+}
+
+await buildWidget();
+
+Script.setWidget(widget);
+Script.complete();
+widget.presentSmall();
